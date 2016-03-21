2a8e045cd24836111fed5ac207053e8e78807faf
sppc <simao.chumbinho@gmail.com>
2016-03-21 21:50:54 +0000

    Homework

---
+++ RestaurantManager.Models/DataManager.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace RestaurantManager.Models
+{
+    public abstract class DataManager : System.ComponentModel.INotifyPropertyChanged
+    {
+        protected RestaurantContext Repository { get; private set; }
+
+        public DataManager()
+        {
+            LoadData();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private async void LoadData()
+        {
+            this.Repository = new RestaurantContext();
+            await this.Repository.InitializeContextAsync();
+            OnDataLoaded();
+        }
+
+        protected abstract void OnDataLoaded();
+    }
+}
